a7177f385968c4d5cb8bd782c8b265fb813cf3ff
dom <dominik.hoch.1989@freenet.de>
2026-01-10 17:01:05 +0100

    switched to commandline arguments for parameters

--- main.py
+++ main.py
@@ -3,77 +3,40 @@ import re
 from _lib.logger import setup_logging
 import logging
 import readline
+from _lib.paramters import Parameters, parsing_for_parameters
+import argparse
 
 def main():
     setup_logging("logs/", "", add_date_to_name=True)
 
     print("Mqtt-Publisher")
-    broker_set = False
+
+    # default values
+    parameters = Parameters(False, "", "logs/logfile.txt", "192.168.178.100", 1884, "climate/office/+", 0, False)
+
+    # parsing command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-x", action="store_true", help="activate fileoutput")
+    parser.add_argument("-o", help= "name or full path of logfile")
+    parser.add_argument("-i", help= "IP of MQTT-broker")
+    parser.add_argument("-p", help= "port of MQTT-broker")
+    parser.add_argument("-t", help= "topic to subscribe to")
+    parser.add_argument("-q", help= "quality of service")
+    parser.add_argument("-r", action="store_true", help= "activate retain message")
+    args = parser.parse_args()
+
+    parsing_for_parameters(args, parameters)
+
+    print(parameters.__repr__())
+
 
     while(True):
-        if broker_set is False:
-            print("Set IP and port for Broker (default 192.168.178.100:1883):")
-            while(True):
-                broker = input()
-                if broker == "":
-                    hostname = "192.168.178.100"
-                    port = 1883
-                    break
-                else:
-                    if re.match(r"[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}:[\d]{1,4}", broker):
-                        hostname, port = broker.split(":")
-                        port = int(port)
-                        break
-                    else:
-                        print("Not the right format! See given example.")
-                        continue
-
-            print("Set the topic to publish to:")
-            while(True):
-                topic = input()
-                if topic == "":
-                    print("Topic must not be empty!")
-                    continue
-                else:
-                    break
-
-            print("Set the quality of service level (default 0):")
-            while(True):
-                qos = input()
-                if qos == "":
-                    qos = 0
-                    break
-                else:
-                    if qos in ["0", "1", "2"]:
-                        break
-                    else:
-                        print("Only values 0, 1 and 2 allowed!")
-                        continue
-
-            print("Set retain True or False (default False):")
-            while(True):
-                retain = input()
-                retain = retain.lower()
-                if retain == "":
-                    retain = False
-                    break
-                else:
-                    if retain in ["true", "false", "1", "0"]:
-                        if retain == "true" or retain == "1":
-                            retain = True
-                        else:
-                            retain = False
-                        break
-                    else:
-                        print("Only true or false allowed!")
-                        continue
-            logging.info(f"[broker: {hostname}:{port}][topic: {topic}][qos: {qos}][retain: {retain}]")
-            broker_set = True
+        
         try:
             print("Type in the message:")
             payload = input()
             logging.info(f"[message: {payload}]")
-            publish.single(topic, payload, int(qos), retain, hostname, port)
+            publish.single(parameters.topic, payload, parameters.qos, parameters.retain, parameters.broker_IP, parameters.broker_port)
         except KeyboardInterrupt:
             break
 
